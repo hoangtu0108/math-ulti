f764b4a2afaff519dab2ab410066334441985328
hoangtu0108 <phamhoangtu6705@gmail.com>
2021-03-17 10:01:12 +0700

    1st upload to GitHub

---
+++ src/MathUltil.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hoang Tu
+ */
+public class MathUltil {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello Github.");
+        System.out.println("This is 1st upload connects to remote GitHub Server");
+                
+    }
+            
+}
